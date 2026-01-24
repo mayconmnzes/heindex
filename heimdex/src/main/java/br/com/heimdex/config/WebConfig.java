9a8d30a5fc18c08a544e9d2c0c9decd289e5bf00
mayconmnzes <mayconmnzes@gmail.com>
2026-01-24 07:37:29 -0300

    Update WebConfig.java

--- heimdex/src/main/java/br/com/heimdex/config/WebConfig.java
+++ heimdex/src/main/java/br/com/heimdex/config/WebConfig.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/uploaded-photos/**")
+            .addResourceLocations("file:./uploaded-photos/");
+}
     /**
      * Configuração global de CORS.
      * Substituímos IPs fixos por padrões que funcionam tanto localmente quanto na nuvem.
